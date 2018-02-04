90ca4dd4f6ed9ca3967e448cfa4dfd1e2cd0c6dc
Robertarg72 <robertarg72@gmail.com>
2018-02-04 00:18:08 -0500

    Add header with team members

--- Slot Machine/ViewController.swift
+++ Slot Machine/ViewController.swift
@@ -1,15 +1,22 @@
-//
-//  ViewController.swift
-//  Slot Machine
-//
-//  Created by Kamalpreet Singh on 2018-01-29.
-//  Copyright © 2018 SlotMachine. All rights reserved.
-//
+/*
+ * ViewController.swift
+ * Project: Assignment 1 - Swift Slot Machine
+ * Students:
+ *          Mehmet Fatih Inan       300966544
+ *          Kamalpreet Singh        300976062
+ *          Robert Argume           300949529
+ *
+ * Date: Feb 05, 2018
+ * Description: Basic Slot Machine for iOS based on "Slot Machine Code" provided on
+ *              GitHub as a template
+ * Version: 0.1
+ */
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    // OUTLETS
     @IBOutlet weak var leftReel: UIImageView!
     
     @IBOutlet weak var middleReel: UIImageView!
@@ -22,16 +29,14 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var betField: UITextField!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
+    
+    // ACTIONS
     @IBOutlet weak var onResetButtonPressed: UIButton!
     
     @IBOutlet weak var onSpinButtonPressed: UIButton!
